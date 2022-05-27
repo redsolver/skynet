5480dcc1aa34be552a66857af03945a31f77833e
redsolver <info@redsolver.net>
2022-05-27 14:16:56 +0200

    generate code

--- lib/src/skystandards/fs.g.dart
+++ lib/src/skystandards/fs.g.dart
@@ -6,20 +6,20 @@ part of 'fs.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-DirectoryIndex _$DirectoryIndexFromJson(Map<String, dynamic> json) {
-  return DirectoryIndex(
-    directories: (json['directories'] as Map<String, dynamic>).map(
-      (k, e) =>
-          MapEntry(k, DirectoryDirectory.fromJson(e as Map<String, dynamic>)),
-    ),
-    files: (json['files'] as Map<String, dynamic>).map(
-      (k, e) => MapEntry(k, DirectoryFile.fromJson(e as Map<String, dynamic>)),
-    ),
-    index: json['index'] as Map<String, dynamic>?,
-    $schema: json[r'$schema'] as String? ??
-        'https://skystandards.hns.siasky.net/draft-01/directoryIndex.schema.json',
-  );
-}
+DirectoryIndex _$DirectoryIndexFromJson(Map<String, dynamic> json) =>
+    DirectoryIndex(
+      directories: (json['directories'] as Map<String, dynamic>).map(
+        (k, e) =>
+            MapEntry(k, DirectoryDirectory.fromJson(e as Map<String, dynamic>)),
+      ),
+      files: (json['files'] as Map<String, dynamic>).map(
+        (k, e) =>
+            MapEntry(k, DirectoryFile.fromJson(e as Map<String, dynamic>)),
+      ),
+      index: json['index'] as Map<String, dynamic>?,
+      $schema: json[r'$schema'] as String? ??
+          'https://skystandards.hns.siasky.net/draft-01/directoryIndex.schema.json',
+    );
 
 Map<String, dynamic> _$DirectoryIndexToJson(DirectoryIndex instance) {
   final val = <String, dynamic>{
@@ -35,16 +35,14 @@ Map<String, dynamic> _$DirectoryIndexToJson(DirectoryIndex instance) {
   }
 
   writeNotNull('index', instance.index);
-
   return val;
 }
 
-DirectoryDirectory _$DirectoryDirectoryFromJson(Map<String, dynamic> json) {
-  return DirectoryDirectory(
-    name: json['name'] as String,
-    created: json['created'] as int,
-  );
-}
+DirectoryDirectory _$DirectoryDirectoryFromJson(Map<String, dynamic> json) =>
+    DirectoryDirectory(
+      name: json['name'] as String,
+      created: json['created'] as int,
+    );
 
 Map<String, dynamic> _$DirectoryDirectoryToJson(DirectoryDirectory instance) =>
     <String, dynamic>{
@@ -52,27 +50,23 @@ Map<String, dynamic> _$DirectoryDirectoryToJson(DirectoryDirectory instance) =>
       'name': instance.name,
     };
 
-DirectoryFile _$DirectoryFileFromJson(Map<String, dynamic> json) {
-  return DirectoryFile(
-    ext: json['ext']?.cast<String, dynamic>(),
-    name: json['name'] as String,
-    created: json['created'] as int,
-    modified: json['modified'] as int,
-    version: json['version'] as int,
-    file: FileData.fromJson(json['file'] as Map<String, dynamic>),
-    history: (json['history'] as Map<String, dynamic>?)?.map(
-          (k, e) => MapEntry(k, FileData.fromJson(e as Map<String, dynamic>)),
-        ) ??
-        {},
-    mimeType: json['mimeType'] as String?,
-  );
-}
+DirectoryFile _$DirectoryFileFromJson(Map<String, dynamic> json) =>
+    DirectoryFile(
+      name: json['name'] as String,
+      created: json['created'] as int,
+      modified: json['modified'] as int,
+      version: json['version'] as int,
+      file: FileData.fromJson(json['file'] as Map<String, dynamic>),
+      ext: json['ext'] as Map<String, dynamic>?,
+      history: (json['history'] as Map<String, dynamic>?)?.map(
+            (k, e) => MapEntry(k, FileData.fromJson(e as Map<String, dynamic>)),
+          ) ??
+          {},
+      mimeType: json['mimeType'] as String?,
+    );
 
 Map<String, dynamic> _$DirectoryFileToJson(DirectoryFile instance) {
-  final val = <String, dynamic>{
-    'created': instance.created,
-    'file': instance.file,
-  };
+  final val = <String, dynamic>{};
 
   void writeNotNull(String key, dynamic value) {
     if (value != null) {
@@ -80,49 +74,49 @@ Map<String, dynamic> _$DirectoryFileToJson(DirectoryFile instance) {
     }
   }
 
+  writeNotNull('ext', instance.ext);
+  val['created'] = instance.created;
+  val['file'] = instance.file;
   writeNotNull('history', instance.history);
   writeNotNull('mimeType', instance.mimeType);
-  writeNotNull('ext', instance.ext);
   val['modified'] = instance.modified;
   val['name'] = instance.name;
   val['version'] = instance.version;
   return val;
 }
 
-FileData _$FileDataFromJson(Map<String, dynamic> json) {
-  return FileData(
-    ext: json['ext']?.cast<String, dynamic>(),
-    chunkSize: json['chunkSize'] as int?,
-    padding: (json['padding'] ?? 0) as int?,
-    encryptionType: json['encryptionType'] as String?,
-    hash: json['hash'] as String,
-    key: json['key'] as String?,
-    ts: json['ts'] as int,
-    url: json['url'] as String,
-    size: json['size'] as int,
-    hashes: json['hashes']?.cast<String>(),
-  );
-}
+FileData _$FileDataFromJson(Map<String, dynamic> json) => FileData(
+      chunkSize: json['chunkSize'] as int?,
+      encryptionType: json['encryptionType'] as String?,
+      hash: json['hash'] as String,
+      key: json['key'] as String?,
+      ts: json['ts'] as int,
+      url: json['url'] as String,
+      size: json['size'] as int,
+      padding: json['padding'] as int? ?? 0,
+      ext: json['ext'] as Map<String, dynamic>?,
+      hashes:
+          (json['hashes'] as List<dynamic>?)?.map((e) => e as String).toList(),
+    );
 
 Map<String, dynamic> _$FileDataToJson(FileData instance) {
-  final val = <String, dynamic>{
-    'size': instance.size,
-    'hash': instance.hash,
-    'ts': instance.ts,
-    'url': instance.url,
-  };
+  final val = <String, dynamic>{};
 
   void writeNotNull(String key, dynamic value) {
     if (value != null) {
       val[key] = value;
     }
   }
+
+  writeNotNull('ext', instance.ext);
+  val['size'] = instance.size;
   writeNotNull('chunkSize', instance.chunkSize);
   writeNotNull('padding', instance.padding);
   writeNotNull('encryptionType', instance.encryptionType);
-  writeNotNull('key', instance.key);
-
-  writeNotNull('ext', instance.ext);
+  val['hash'] = instance.hash;
   writeNotNull('hashes', instance.hashes);
+  writeNotNull('key', instance.key);
+  val['ts'] = instance.ts;
+  val['url'] = instance.url;
   return val;
 }
