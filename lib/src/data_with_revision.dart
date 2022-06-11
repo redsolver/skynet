34643d31611b654d5b3557d02b4b4d16308cb617
redsolver <info@redsolver.net>
2022-06-11 15:41:02 +0200

    Add skylink field for registry data

--- lib/src/data_with_revision.dart
+++ lib/src/data_with_revision.dart
@@ -1,7 +1,8 @@
 class DataWithRevision<T> {
   final T data;
   final int revision;
-  DataWithRevision(this.data, this.revision);
+  final String? skylink;
+  DataWithRevision(this.data, this.revision, {this.skylink});
 
   @override
   String toString() => 'DataWithRevision<$T>(revision: $revision, data: $data)';
